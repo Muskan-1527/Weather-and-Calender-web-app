6ad8949883138bbcffe7b0ccc13350f3756d63fb
ShristiGup <guptashristi25@gmail.com>
2019-09-15 11:16:27 +0530

    authentication added

--- weather_and_calendar_web_app/src/App.js
+++ weather_and_calendar_web_app/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import fire from './config/config';
 import Buildcontrols from './components/Buildcontrols/Buildcontrols';
+import Calendar from './components/Calendar/Calendar';
+import Login from './components/Login/Login';
+
 class App extends Component {
-  render() {
+constructor(props){
+  super(props);
+    this.state = {
+      user:{},
+    }
+  }
+  
+componentDidMount = () =>{
+  this.authEventHandler();
+}
+
+authEventHandler = () => {
+  fire.auth().onAuthStateChanged((user) => {
+    if(user){
+      this.setState({user});
+    }
+    else{
+      this.setState({user:null});
+    }
+  });
+}
+  render = () => {
     return (
      <div>
        <Buildcontrols/>
+       {this.state.user ? (<Calendar/>) :(<Login/>) }
      </div>
     );
   }
--- weather_and_calendar_web_app/src/components/Content/Content.js
+++ weather_and_calendar_web_app/src/components/Content/Content.js
@@ -8,15 +8,31 @@ const content = (props) =>(
        <div className="row">
            <div className="col-xs-12">
            <h1 className={classes.Content}>Weather App</h1>
+
+           
+           </div>
+       </div>
+       <div className="row">
+           <div className="col-xs-12">
+          
            <h3 className={classes.Tagline}>Come for the weather,Stay for the experience...</h3>
            
            </div>
        </div>
        <div className="row">
            <div className="col-xs-12">
+           <h1 className={classes.Calendar}>Welcome to Calendar</h1>
+           
            
            </div>
        </div>
+
+
+       {/* <div className="row">
+           <div className="col-xs-12">
+           
+           </div>
+       </div> */}
    
    {/* <div className="row">
            <div className="col-xs-12">
