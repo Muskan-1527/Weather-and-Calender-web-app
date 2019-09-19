73d4792347a95121f823c15f302968a9a9ddc7f7
muskan-1527 <agrawalm8832gmail.com>
2019-09-19 13:04:07 +0530

    calendar structure

--- weather_and_calendar_web_app/src/calendar_app_component/calendar_app.js
+++ weather_and_calendar_web_app/src/calendar_app_component/calendar_app.js
@@ -2,13 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import './calendar_app.css';
 
-class Calendar_app extends React.Component {
-    constructor(props) {
-        super(props);
-        this.width = props.width || "350px";
-        this.style = props.style || {};
-    }
-
+export default class Calendar extends React.Component {
     state = {
         dateContext: moment(),
         today: moment(),
@@ -16,58 +10,119 @@ class Calendar_app extends React.Component {
         showYearPopup: false
     }
 
-    weekdays = moment.weekdays();
-    weekdaysShort = moment.weekdaysShort();
+    constructor(props) {
+        super(props);
+        this.width = props.width || "350px";
+        this.style = props.style || {};
+
+    }
+
+
+    weekdays = moment.weekdays(); //["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"]
+    weekdaysShort = moment.weekdaysShort(); // ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     months = moment.months();
 
     year = () => {
         return this.state.dateContext.format("Y");
     }
-
     month = () => {
         return this.state.dateContext.format("MMMM");
     }
-
     daysInMonth = () => {
-        return this.state.dateContext.dateInMonth();
+        return this.state.dateContext.daysInMonth();
     }
-
     currentDate = () => {
+        console.log("currentDate: ", this.state.dateContext.get("date"));
         return this.state.dateContext.get("date");
     }
-
     currentDay = () => {
         return this.state.dateContext.format("D");
     }
 
     firstDayOfMonth = () => {
         let dateContext = this.state.dateContext;
-        let firstDay = moment(dateContext).startOf('month').format('d');
+        let firstDay = moment(dateContext).startOf('month').format('d'); // Day of week 0...1..5...6
+        return firstDay;
     }
-    
-    render() {
 
+    render() {
+        // Map the weekdays i.e Sun, Mon, Tue etc as <td>
         let weekdays = this.weekdaysShort.map((day) => {
             return (
                 <td key={day} className="week-day">{day}</td>
             )
         });
+
+        let blanks = [];
+        for (let i = 0; i < this.firstDayOfMonth(); i++) {
+            blanks.push(<td key={i * 80} className="emptySlot">
+                {""}
+                </td>
+            );
+        }
+
+        console.log("blanks: ", blanks);
+
+        let daysInMonth = [];
+        for (let d = 1; d <= this.daysInMonth(); d++) {
+            let className = (d === this.currentDay() ? "day current-day": "day");
+            let selectedClass = (d === this.state.selectedDay ? " selected-day " : "")
+            daysInMonth.push(
+                <td key={d} className={className + selectedClass} >
+                    <span>{d}</span>
+                </td>
+            );
+        }
+
+
+        console.log("days: ", daysInMonth);
+
+        var totalSlots = [...blanks, ...daysInMonth];
+        let rows = [];
+        let cells = [];
+
+        totalSlots.forEach((row, i) => {
+            if ((i % 7) !== 0) {
+                cells.push(row);
+            } else {
+                let insertRow = cells.slice();
+                rows.push(insertRow);
+                cells = [];
+                cells.push(row);
+            }
+            if (i === totalSlots.length - 1) {
+                let insertRow = cells.slice();
+                rows.push(insertRow);
+            }
+        });
+
+        let trElems = rows.map((d, i) => {
+            return (
+                <tr key={i*100}>
+                    {d}
+                </tr>
+            );
+        })
+
         return (
-            <div className = "calendar-container">
+            <div className="calendar-container" style={this.style}>
                 <table className="calendar">
                     <thead>
                         <tr className="calendar-header">
+                        
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             {weekdays}
                         </tr>
+                        {trElems}
                     </tbody>
                 </table>
+
             </div>
+
         );
     }
 }
 
-export default Calendar_app;
